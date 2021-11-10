2758e701d491e66ec18eecde517aa100f2e33fe0
Kendall Goto <k@kgo.to>
2021-11-10 16:58:29 -0500

    add admin view, refactor project slug

--- frontend/router.js
+++ frontend/router.js
@@ -6,13 +6,13 @@ router.get('/', function (req, res, next) {
 	res.render('landing', { title: "Manag.ly - Find your Project" });
 });
 
-router.get('/:proj', function (req, res, next) {
+router.get('/pr/:proj', function (req, res, next) {
 	console.log(req);
-	res.render('project', { title: "Manag.ly - Project View", active: "project", slug: `/${req.params.proj}` });
+	res.render('project', { title: "Manag.ly - Project View", active: "project", slug: `/pr/${req.params.proj}` });
 });
 
-router.get('/:proj/team', function (req, res, next) {
-	res.render('team', { title: "Manag.ly - Team View", active: "team", slug: `/${req.params.proj}` });
+router.get('/pr/:proj/team', function (req, res, next) {
+	res.render('team', { title: "Manag.ly - Team View", active: "team", slug: `/pr/${req.params.proj}` });
 });
 
 router.get('/admin', function (req, res, next) {
