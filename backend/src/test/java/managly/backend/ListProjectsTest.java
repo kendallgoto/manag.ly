f6c25667bd68ae1909c7b7f98910f0a189e94b79
ngrumski <grumski88@gmail.com>
2021-11-29 14:08:45 -0500

    sample test code

--- backend/src/test/java/managly/backend/ListProjectsTest.java
+++ backend/src/test/java/managly/backend/ListProjectsTest.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.gson.Gson;
+
+import managly.backend.http.*;
+
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
@@ -12,7 +16,11 @@ public class ListProjectsTest extends LambdaTest {
 	
     @Test
     public void testGetList() throws IOException {
-    	Assert.assertTrue(true);
+
+    	CreateProjectHandler handler = new CreateProjectHandler();
+    	CreateProjectRequest req = new Gson().fromJson( "test", CreateProjectRequest.class);
+    	ManaglyResponse response = handler.handleRequest(req, createContext(""));
+    	Assert.assertEquals(response.toString(),new GenericErrorResponse(200, "OK!").toString());
     }
 
 }
