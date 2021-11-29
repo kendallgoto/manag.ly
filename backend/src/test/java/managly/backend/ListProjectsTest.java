9168e0d9b30f3bb7233c1bb710aa3a23684fe2fd
Kendall Goto <k@kgo.to>
2021-11-29 02:58:12 -0500

    Restore generic lambda tests

---
+++ backend/src/test/java/managly/backend/ListProjectsTest.java
@@ -0,0 +1,18 @@
+package managly.backend;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class ListProjectsTest extends LambdaTest {
+	
+    @Test
+    public void testGetList() throws IOException {
+    	Assert.assertTrue(true);
+    }
+
+}
