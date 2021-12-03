ead1a22c873547d8ef6f581b5ca4316a4754c422
Nick Hom <nchom@wpi.edu>
2021-12-02 21:19:43 -0500

    DeleteProjectHandler and test cases

--- backend/src/main/java/managly/backend/DeleteProjectHandler.java
+++ backend/src/main/java/managly/backend/DeleteProjectHandler.java
@@ -1,9 +1,13 @@
 package managly.backend;
 
+import java.sql.SQLException;
+
 import com.amazonaws.services.lambda.runtime.*;
 
 import managly.backend.http.ManaglyResponse;
 import managly.backend.http.ProjectRequest;
+import managly.backend.http.ProjectResponse;
+import managly.backend.db.ProjectDocument;
 import managly.backend.http.GenericErrorResponse;
 
 
@@ -16,6 +20,21 @@ public class DeleteProjectHandler implements RequestHandler<ProjectRequest, Mana
 		logger = context.getLogger();
 		logger.log("Handling DeleteProjectHandler");
 		logger.log(req.toString());
+		
+		ProjectDocument deletedProj = new ProjectDocument();
+		try {
+			if(deletedProj.save()) {
+				if(deletedProj.save()) {
+					logger.log("Project found "+deletedProj.getObject().getId());
+					return new ProjectResponse(deletedProj);
+			} else {
+			throw GenericErrorResponse.error(404, context, "Project does not exist");
+		}
+		}catch(SQLException e) {
+			e.printStackTrace();
+			throw GenericErrorResponse.error(500, context, "some SQL error occured");
+		}
+	
 
 		return null;
 	}
--- backend/src/test/java/managly/backend/DeleteProjectTest.java
+++ backend/src/test/java/managly/backend/DeleteProjectTest.java
@@ -1,6 +1,7 @@
 package managly.backend;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 
 import com.google.gson.Gson;
 
+import managly.backend.db.ProjectDocument;
 import managly.backend.http.*;
 
 /**
@@ -16,7 +18,7 @@ import managly.backend.http.*;
  */
 public class DeleteProjectTest extends LambdaTest {	
     @Test
-    public void deleteProject() throws IOException {
+    public void deleteProject() throws IOException, SQLException {
     	//Create Project
     	//Create Task + add to Project
     	//Create subtask + add to Project
@@ -24,10 +26,39 @@ public class DeleteProjectTest extends LambdaTest {
     	//Create TaskAssignment + add to Project
     	//Delete Project
     	//Verify Teammate, Task, Subtask, TaskAssignment are deleted.
+    	ProjectResponse newProj = new CreateProjectTest().testGoodCreate();
+    	int projId = newProj.getId();
+    	ProjectDocument deletedProject = new ProjectDocument("Test Project");
+    	DeleteProjectHandler deleteHandler = new DeleteProjectHandler();
+    	AddTeammateHandler teamHandler = new AddTeammateHandler();
+    	TeammateRequest teamReq = new Gson().fromJson( "{\"name\":\"test name\", \"projectId\":"+newProj.getId() +"}", ProjectRequest.class);
+    	ManaglyResponse teamResponse = teamHandler.handleRequest(teamReq, createContext(""));
+    	AddTaskHandler taskHandler = new AddTaskHandler();
+    	TaskRequest createTaskReq = new Gson().fromJson( "{\"name\":\"My Super Cool First Task\", \"projectId\": "+projId+"}", TaskRequest.class);
+    	ManaglyResponse taskResponse = taskHandler.handleRequest(createTaskReq, createContext(""));
+    	deletedProject.delete();
+    	ProjectRequest findReq = new Gson().fromJson( "{\"projectId\":99}", ProjectRequest.class);
+
+    	
+    	Assertions.assertThrows(GenericErrorResponse.class, () -> {
+	    	deleteHandler.handleRequest(findReq, createContext(""));
+		});
+    	Assertions.assertThrows(GenericErrorResponse.class, () -> {
+	    	teamHandler.handleRequest(teamReq, createContext(""));
+		});
+    	Assertions.assertThrows(GenericErrorResponse.class, () -> {
+	    	taskHandler.handleRequest(createTaskReq, createContext(""));
+		});
+    	
     }
     
     @Test
     public void deleteMissingProject() throws IOException {
     	//Call delete + fail
+    	DeleteProjectHandler findProject = new DeleteProjectHandler();
+    	ProjectRequest findReq = new Gson().fromJson( "{\"projectId\":99}", ProjectRequest.class);
+		Assertions.assertThrows(GenericErrorResponse.class, () -> {
+	    	findProject.handleRequest(findReq, createContext(""));
+		});
     }
 }
