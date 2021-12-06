265ea97682d8378fe2a27e117fa814f619a65073
wstanley <wstanley@wpi.edu>
2021-12-06 13:43:21 -0500

    Make files for ArchiveTaskHander MarkTaskHandler and RenameTaskHandler

---
+++ backend/src/main/java/managly/backend/MarkTaskHandler.java
@@ -0,0 +1,25 @@
+package managly.backend;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import com.amazonaws.services.lambda.runtime.*;
+
+import managly.backend.http.ManaglyResponse;
+import managly.backend.http.TeammateRequest;
+import managly.backend.http.TeammateResponse;
+import managly.backend.db.ProjectDocument;
+import managly.backend.db.TeammateDocument;
+import managly.backend.http.GenericErrorResponse;
+
+public class MarkTaskHandler implements RequestHandler<TaskRequest, ManaglyResponse> {
+	
+	public LambdaLogger logger;
+	
+	@Override
+	public ManaglyResponse handleRequest(TeammateRequest req, Context context) {
+		logger = context.getLogger();
+		logger.log("Handling AddTeammateHandler");
+		logger.log(req.toString());
+	
+}
