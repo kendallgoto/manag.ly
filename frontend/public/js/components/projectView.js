55628cece534a89be9004278b17b9db6530f75b6
Kendall Goto <k@kgo.to>
2021-12-09 20:36:50 -0500

    fix nesting, add task marking

--- frontend/public/js/components/projectView.js
+++ frontend/public/js/components/projectView.js
@@ -7,7 +7,6 @@ class ProjectView {
 		this.$projectTaskTemplate = $("#projectView #projectTaskTemplate");
 		this.$addTaskTemplate = $("#projectView #addTaskTemplate");
 		this.fetchProject(window.projectId);
-		this.registerHandlers();
 	}
 	renderProject(project) {
 		this.$navProjTitle.text(project.title);
@@ -28,38 +27,44 @@ class ProjectView {
 	renderTask(task, parent) {
 		const $addTo = (parent) ? parent : this.$projectTaskList;
 		const $thisTask = this.$projectTaskTemplate.clone();
-		$thisTask.removeAttr('id').removeClass('Template');
+		$thisTask.insertBefore($('> .task-entry-add', $addTo));
+		$thisTask.removeAttr('id').removeClass('Template').data('taskId', task.id);
 		if (task.completed) {
-			$('.task-completion', $thisTask).html(`<span data-feather="check-square"></span>`);
+			$('> .task-completion', $thisTask).html(`<span data-feather="check-square"></span>`);
 			$thisTask.addClass('task-done');
+		} else {
+			$('> .task-completion', $thisTask).html(`<span data-feather="square"></span>`);
+			$thisTask.removeClass('task-done');
 		}
-		$('.task-number', $thisTask).text(task.taskNumber);
-		$('.task-label', $thisTask).text(task.name);
-		if (!this.subtasks && task.assigned) {
+		$('> .task-number', $thisTask).text(task.taskNumber);
+		$('> .task-label', $thisTask).text(task.name);
+		if (!task.subtasks?.length && task.assigned) {
 			for (const assignee of task.assigned) {
-				const $thisAssign = $('.task-assign', $thisTask).clone();
+				const $thisAssign = $('> .task-assign', $thisTask).clone();
 				$thisAssign.removeClass('task-assign');
 				$thisAssign.text(assignee.name);
 				$thisAssign.attr('title', assignee.name);
-				$thisAssign.prependTo($('.task-assignment-box', $thisTask));
+				$thisAssign.prependTo($('> .task-assignment-box', $thisTask));
 			}
 		}
-		if (this.subtasks) {
-			$('.task-assignment-box', $thisTask).remove();
+		if (task.subtasks?.length) {
+			$('> .task-completion', $thisTask).remove();
+			$('> .task-assignments .task-assignment-box', $thisTask).remove();
 			$thisTask.addClass('task-withSubs');
-			const $subtaskBox = $('.task-subtasks', $thisTask);
-			for (const subtask of this.subtasks) {
+			const $subtaskBox = $('> .task-subtasks', $thisTask);
+			for (const subtask of task.subtasks) {
 				this.renderTask(subtask, $subtaskBox);
 			}
-		} else $('.task-subtasks', $thisTask).remove();
+		} else $('> .task-subtasks', $thisTask).remove();
 
-		$thisTask.insertBefore($('.task-entry-add', $addTo));
 	}
 	fetchProject() {
-		$.get('/projects/'+window.projectId).done( (project) => {
+		$.get('/projects/' + window.projectId).done((project) => {
 			console.log(project);
 			this.renderProject(project);
-		}).fail( (err) => {
+			window.feather.replace();
+			this.registerHandlers();
+		}).fail((err) => {
 			this.$navProjTitle.text("404: Project not found!");
 			console.error("Failed to retrieve project", err);
 		});
@@ -75,7 +80,6 @@ class ProjectView {
 		const parentId = $parent.data('taskId');
 		const projectId = window.projectId;
 
-		console.log("will add new task", newTask);
 		$.post('/tasks', JSON.stringify({
 			name: newTask,
 			projectId: projectId,
@@ -84,6 +88,7 @@ class ProjectView {
 			console.log("Created new task", resp);
 			$taskAdder.remove();
 			this.renderTask(resp, $parent);
+			this.registerHandlers();
 		}).fail((err) => {
 			console.log("Failed to create new task", err);
 			$taskField
@@ -99,7 +104,7 @@ class ProjectView {
 		if ($alreadyAdding.length) {
 			console.log("already adding in progress!");
 			$alreadyAdding.removeClass('animate__animated animate__flash');
-			setTimeout(function() { // allow for page refresh
+			setTimeout(function () { // allow for page refresh
 				$alreadyAdding.addClass('animate__animated animate__flash');
 			}, 5);
 			return;
@@ -113,7 +118,7 @@ class ProjectView {
 		console.log(firstPath);
 		let pathArray = firstPath.split('.');
 		pathArray = pathArray.slice(0, -2); // remove last two elements
-		pathArray.push($knownTasks.length+1);
+		pathArray.push($knownTasks.length + 1);
 		pathArray.push("");
 		const finalPath = pathArray.join('.');
 		$('.task-number', $thisToAdd).text(finalPath);
@@ -123,10 +128,29 @@ class ProjectView {
 		});
 		$thisToAdd.insertBefore($addButton);
 	}
+	markTask($task) {
+		const $completion = $('> .task-completion', $task);
+		if ($completion.hasClass('disabled')) {
+			return;
+		}
+		$completion.addClass('disabled', true);
+		const isComplete = (!($('> .feather-check-square', $completion).length)).toString;
+		const taskId = $task.data('taskId');
+
+		$.post('/tasks/' + taskId + '/completed/' + isComplete).done((resp) => {
+			console.log("Update marking", resp);
+		}).fail((err) => {
+			console.log("Failed to mark", err);
+		});
+	}
 	registerHandlers() {
 		$('.task-entry-add').off('click').on('click', (e) => {
 			const $addTo = $(e.currentTarget);
 			this.setupTaskAdder($addTo);
+		});
+		$('.task-completion').off('click').on('click', (e) => {
+			const $taskToMark = $(e.currentTarget).closest('.task-entry');
+			this.markTask($taskToMark)
 		});
 	}
 }
