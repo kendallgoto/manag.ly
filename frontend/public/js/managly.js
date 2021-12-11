278bd0d966e8e4f18d7b6f6d28c6b635ce83599a
Kendall Goto <k@kgo.to>
2021-12-10 21:24:38 -0500

    switch api endpoint to prod for prod

--- frontend/public/js/managly.js
+++ frontend/public/js/managly.js
@@ -3,10 +3,10 @@ import { init } from './components/pages.js';
 (function () {
 	$.ajaxSetup({
 		dataType: "json",
-		beforeSend: function(xhr, options) {
-			options.url = "https://y1ix8qw498.execute-api.us-east-1.amazonaws.com/staging" + options.url;
+		beforeSend: function (xhr, options) {
+			options.url = "https://y1ix8qw498.execute-api.us-east-1.amazonaws.com/prod" + options.url;
 		}
-	});	
+	});
 	$(function () {
 		window.feather.replace();
 		init();
