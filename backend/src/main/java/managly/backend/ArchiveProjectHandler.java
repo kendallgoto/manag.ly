c2e338bc47a83f45d04de1b315cbcbdaa7f60d7e
Kendall Goto <k@kgo.to>
2021-12-10 19:44:00 -0500

    tidy archiveProjectHandler

--- backend/src/main/java/managly/backend/ArchiveProjectHandler.java
+++ backend/src/main/java/managly/backend/ArchiveProjectHandler.java
@@ -6,39 +6,37 @@ import java.sql.SQLIntegrityConstraintViolationException;
 import com.amazonaws.services.lambda.runtime.*;
 
 import managly.backend.http.ManaglyResponse;
+import managly.backend.http.ProjectRequest;
 import managly.backend.http.TeammateRequest;
 import managly.backend.http.TeammateResponse;
 import managly.backend.db.ProjectDocument;
 import managly.backend.db.TeammateDocument;
 import managly.backend.http.GenericErrorResponse;
+import managly.backend.http.GenericSuccessResponse;
 
-public class ArchiveTaskHandler implements RequestHandler<TaskRequest, ManaglyResponse> {
+public class ArchiveProjectHandler implements RequestHandler<ProjectRequest, ManaglyResponse> {
 	
 	public LambdaLogger logger;
 	
 	@Override
-	public ManaglyResponse handleRequest(TeammateRequest req, Context context) {
+	public ManaglyResponse handleRequest(ProjectRequest req, Context context) {
 		logger = context.getLogger();
-		logger.log("Handling AddTeammateHandler");
+		logger.log("Handling ArchiveProjectHandler");
 		logger.log(req.toString());
 		
 		ProjectDocument existingProj = new ProjectDocument();
-
 		try {
-			if(!existingProj.getObject().isArchived()) {
-				existingProj.getObject().setArchived(true);
-				if(existingProj.save()) {
-					logger.log("Project is successfully marked archived.");
-					return new ProjectResponse(existingTask);
-				} else {
+			if(existingProj.findById(req.getProjectId())) {
+				if(!existingProj.getObject().isArchived()) {
+					existingProj.getObject().setArchived(true);
+					if(existingProj.save()) {
+						return new GenericSuccessResponse(204, "Project updated successfully");
+					}
 					throw GenericErrorResponse.error(500, context, "Uncaught saving error");
 				}
-			} else {
 				throw GenericErrorResponse.error(403, context, "Project is already archived.");
 			}
-		 else {
 			throw GenericErrorResponse.error(404, context, "Project not found");
-		}
 		} catch(SQLException e) {
 			e.printStackTrace();
 			throw GenericErrorResponse.error(500, context, "Uncaught MySQL error");
